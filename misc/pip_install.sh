ce52c12fc76f826c4c2bb18e413c9596c5340051
gowerc <craiggower@gmail.com>
2021-08-29 19:03:28 +0100

    updated python reirements

--- misc/pip_install.sh
+++ misc/pip_install.sh
@@ -11,4 +11,11 @@ python -m pip install google-auth
 python -m pip install google-api-python-client
 python -m pip install numpy
 python -m pip install gunicorn
+python -m pip install google-cloud
+python -m pip install google-oauth2
+python -m pip install flake8
+python -m pip install requests
+python -m pip install wheel
+
+python -m pip freeze > requirements.txt
 
